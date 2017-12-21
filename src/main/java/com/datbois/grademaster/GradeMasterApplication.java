842bf80b8eba31c90f0dc07558ecfe962edf3814
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-12-21 10:38:44 +0100

    Init empty project

---
+++ src/main/java/com/datbois/grademaster/GradeMasterApplication.java
@@ -0,0 +1,12 @@
+package com.datbois.grademaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeMasterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/datbois/grademaster/GradeMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.datbois.grademaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradeMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
