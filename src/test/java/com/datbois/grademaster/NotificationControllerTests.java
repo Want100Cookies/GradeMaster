a7fc76c3e70d601f91a5edda45874d939be409ef
Danny Hooyer <DannyHooyer@gmail.com>
2018-01-15 14:38:40 +0100

    Updated NotificationController, All NotificationControllerTests working

--- src/test/java/com/datbois/grademaster/NotificationControllerTests.java
+++ src/test/java/com/datbois/grademaster/NotificationControllerTests.java
@@ -1,17 +1,23 @@
 package com.datbois.grademaster;
 
 import com.datbois.grademaster.model.Notification;
+import com.datbois.grademaster.model.User;
 import com.datbois.grademaster.service.NotificationService;
+import com.datbois.grademaster.service.UserService;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 
+import javax.naming.AuthenticationNotSupportedException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.authentication;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -22,9 +28,12 @@ public class NotificationControllerTests extends OAuthTests {
     @Autowired
     private NotificationService notificationService;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void retrieveAllNotifications(){
-        String token = this.obtainAccessToken("admin@stenden.com", "password");
+        String token = this.obtainAccessToken("john.doe@student.stenden.com", "password");
 
         given()
                 .auth()
@@ -36,47 +45,48 @@ public class NotificationControllerTests extends OAuthTests {
     }
 
     @Test
-    @Ignore("Does not work yet")
-    public void updateAllNotificationsSeen(){
-        List<Notification> notifications = notificationService.findAll();
-        String token = this.obtainAccessToken("admin@stenden.com", "password");
+    public void updateAllNotifications(){
+        String token = this.obtainAccessToken("john.doe@student.stenden.com", "password");
+
+        User user = userService.findByEmail("john.doe@student.stenden.com");
+
+        List<Notification> notifications = user.getNotificationList();
 
         given()
                 .auth()
                 .oauth2(token)
-                .contentType(ContentType.JSON)
                 .when()
                 .patch("/api/v1/notifications")
                 .then()
                 .statusCode(HttpStatus.OK.value());
 
-        for(Notification notification : notifications){
-            assertThat(notification.isSeen(), is(equalTo(true)));
+        for(Notification n : notifications){
+            assertThat(n.isSeen(), equalTo(true));
         }
-
     }
 
     @Test
     public void updateNotification(){
-        Notification testNotification = notificationService.findById(Long.parseLong("1"));
-        String token = this.obtainAccessToken("admin@stenden.com", "password");
+        String token = this.obtainAccessToken("john.doe@student.stenden.com", "password");
 
-        Map<String, Boolean> notificationData = new HashMap<>();
-        notificationData.put("seen", true);
+        Notification notification = notificationService.findById(1L);
 
-        given()
+        Map<String, Boolean> data = new HashMap<>();
+        data.put("seen", true);
+
+        HashMap<String, Boolean> result = given()
                 .auth()
                 .oauth2(token)
                 .contentType(ContentType.JSON)
-                .body(notificationData)
+                .body(data)
                 .when()
-                .patch("/api/v1/notifications/{notificationId}", testNotification.getId())
+                .patch("/api/v1/notifications/{notificationId}", notification.getId())
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body("seen", is(notificationData.get("seen")));
+                .extract()
+                .path("$");
 
-        Notification updatedNotification = notificationService.findById(testNotification.getId());
-
-        assertThat("Notification is marked as seen.", updatedNotification.isSeen(), not(equalTo(testNotification.isSeen())));
+        assertThat("Notification - Marked as seen", result.get("seen"), Matchers.is(data.get("seen")));
     }
+
 }
