ebf160e7558000b6feaa70af3e5e3192376bf2ee
jstrating <jesper.strating@outlook.com>
2018-01-17 15:44:47 +0100

    Added create group dialog, Added Autocomplete user-chips for adding students to group

--- src/main/resources/public/app/js/groups.js
+++ src/main/resources/public/app/js/groups.js
@@ -1,36 +1,69 @@
-app.controller('GroupsCtrl', function($scope){
+app.controller('GroupsCtrl', function ($scope) {
 
 });
 
-app.controller('TeacherGroupsCtrl', function($scope, $mdDialog, UserService){
+app.controller('TeacherGroupsCtrl', function ($scope, $mdDialog, UserService) {
     $scope.status = '  ';
-    $scope.userArray = [];
-    $scope.showAddGroup = function(ev) {
-        UserService.getAllUsers().then(function(response){
-            $scope.userArray = response.data;
-        });
-        $mdDialog.show({
-          controller: DialogController,
-          templateUrl: '/app/dialogs/addGroupDialog.tmpl.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose:true,
-          fullscreen: $scope.customFullscreen
-        })
-      };
+    $scope.vm = {
+        formData: {
+            groupName: '',
+            period: [],
+            startYear: '',
+            endYear: '',
+            course: {
+                id: ''
+            },
+            SelectedStudents: [
 
-      function DialogController($scope, $mdDialog) {
-        $scope.hide = function() {
-          $mdDialog.hide();
+            ]
+
+        
+        },
+    }
+    $scope.showAddGroup = function (ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/app/dialogs/addGroupDialog.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: $scope.customFullscreen
+        })
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function () {
+            $mdDialog.hide();
         };
-    
-        $scope.cancel = function() {
-          $mdDialog.cancel();
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
         };
-    
-        $scope.answer = function(answer) {
-          $mdDialog.hide(answer);
-        };
-      }
+    }
 });
 
+app.controller('ChipCtrl', function(UserService){
+    var self = this;
+    self.userArray = [];
+    self.SelectedStudents = [];
+    UserService.getAllUsers().then(function (response) {
+        self.userArray = response.data;
+    });
+
+    self.Search = function (query) {
+        var results = query
+            ? self.userArray.filter(createFilterFor(query))
+            : [];
+
+        return results;
+    }
+
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+
+        return function filterFn(user) {
+            return (angular.lowercase(user.name)
+                .indexOf(lowercaseQuery) != -1);
+        };
+    }
+})
