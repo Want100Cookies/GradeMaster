dc7d335e6c849b5380a0fcecb46a241db381e372
Danny Hooyer <DannyHooyer@gmail.com>
2018-01-19 11:03:04 +0100

    Made finalgrade api call in student-groups-service.js, fixed some styling issues with displaying groupmembernames and grade button

--- src/main/resources/public/app/components/groupCard.component.js
+++ src/main/resources/public/app/components/groupCard.component.js
@@ -2,24 +2,29 @@ function GroupCardCtrl($scope, StudentGroupsService){
     var ctrl = this;
 
     $scope.init = function(){
-        $scope.name =  $scope.$ctrl.group.groupName;
-        $scope.groupStatus = "TEST";
-        $scope.groupGrade = $scope.$ctrl.group.groupGrade.grade;
-        $scope.studentGrade = "MOET NOG";
+        ctrl.name =  $scope.$ctrl.group.groupName;
+        ctrl.groupStatus = "TEST";
+        ctrl.groupGrade = $scope.$ctrl.group.groupGrade.grade;
+
         $scope.status = "pending";
 
         StudentGroupsService.getGroupMembers($scope.$ctrl.group.id).then(function(response){
             $scope.groupMembers = response.data;
         });
 
-        console.log($scope.$ctrl.group);
+        StudentGroupsService.getFinalGroupGrade($scope.$ctrl.group.id, $scope.$ctrl.user.id).then(function(response){
+            $scope.finalGroupGrades = response.data;
+        });
     }
+
+
 }
 
 app.component('groupCard', {
     templateUrl: '/app/components/groupCard.component.html',
     controller: GroupCardCtrl,
     bindings: {
+        user: '<',
         group: '<',
     }
 });
--- src/main/resources/public/app/js/student-groups-service.js
+++ src/main/resources/public/app/js/student-groups-service.js
@@ -32,6 +32,23 @@ app.factory('StudentGroupsService', function($http, $cookies){
             }).catch(function(data){
                 console.log("ERROR", data);
             });
+        },
+
+        getFinalGroupGrade : function(groupId, userId){
+            var accessToken = $cookies.get("access_token");
+            var req = {
+                method: 'GET',
+                url: 'http://localhost:8080/api/v1/grades/groups/'+ groupId +'/users/'+ userId,
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                }
+            }
+            return $http(req).then(function(data){
+                console.log("Succesful getFinalGroupGrade API Call", data);
+                return data;
+            }).catch(function(data){
+                console.log("ERROR", data);
+            });
         }
     }
 });
