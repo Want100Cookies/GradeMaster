b1498fd4b8637fb2fda62b5597eb36ed3cfa5fbe
Jesper Strating <Jesper.strating@outlook.com>
2018-01-19 19:32:52 +0100

    Sql back to old

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO `user` (`id`, `email`, `name`, `password`, `verified`, `reference_id
   (1, "john.doe@student.stenden.com", "John Doe", "$2a$04$FYZXxiv7A74rX33gfs2m/.AGqhQ/unlJCB2nHLRiuHCVlECcyLyb6", 1, '123456'),
   (2, "jane.doe@stenden.com", "Jane Doe", "$2a$04$FYZXxiv7A74rX33gfs2m/.AGqhQ/unlJCB2nHLRiuHCVlECcyLyb6", 1, '654321'),
   (3, "admin@stenden.com", "Administrator", "$2a$04$FYZXxiv7A74rX33gfs2m/.AGqhQ/unlJCB2nHLRiuHCVlECcyLyb6", 1, '987555');
-   
+
 INSERT INTO `role` (`id`, `code`, `label`) VALUES
   (1, "STUDENT_ROLE", "Student"),
   (2, "TEACHER_ROLE", "Teacher"),
@@ -30,6 +30,7 @@ INSERT INTO `group_grade` (`id`,`grade`,`comment`, `teacher_id`) VALUES
 INSERT INTO `group` (`id`, `course_id`, `group_name`, `start_year`, `end_year`, `group_grade_id`) VALUES
   (1, 1, "Musketiers", 2017, 2018, 1),
   (2, 2, "Hackerman", 2017, 2018, 2),
+  (3, 3, "Madam", 2016, 2017, 3);
 
 INSERT INTO `group_periods` (`group_id`, `period`) VALUES
   (1, "Q1"),
@@ -58,7 +59,6 @@ INSERT INTO `notification`(`id`, `title`, `message`, `seen`, `user_id`) VALUES
  (3, "Final", "Your final grade has been determined.", 0, 1),
  (4, "Received", "All group ratings received", 0, 2),
  (5, "Test", "Group grade can be made final", 0, 2);
-
 
 
 
