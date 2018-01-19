2c4b25f8cd5258d471cc1bd23cd23fb32de29b1d
cassshh <casvd@hotmail.com>
2018-01-19 13:50:41 +0100

    Add custom simple api for http requests

---
+++ src/main/resources/public/app/services/api.service.js
@@ -0,0 +1,136 @@
+/**
+ * HTTP API for GradeMaster
+ * 
+ * Simplify http requests
+ * 
+ * Made by Cas van Dinter
+ */
+app.factory('API', function ($cookies, $http, $httpParamSerializer) {
+    /**
+     * CONFIG
+     */
+    const BASE_URL = `http://localhost:8080`;
+    const API = `api/v1`;
+    const OAUTH_KEY = `Z3JhZGVtYXN0ZXItY2xpZW50OmdyYWRlbWFzdGVyLXNlY3JldA==`;
+
+    /**
+     * DEFAULT REQUESTS
+     */
+    const REQUEST = {
+        url: `${BASE_URL}/${API}/`,
+        headers: {
+            'Authorization': `Bearer ${$cookies.get("access_token")}`
+        }
+    };
+    const AUTH_REQUEST = {
+        url: `${BASE_URL}/oauth/token`,
+        headers: {
+            'Authorization': `Basic ${OAUTH_KEY}`,
+            'Content-type': `application/x-www-form-urlencoded; charset=utf-8`
+        },
+    };
+
+    /**
+     * Auth user
+     * @param {*} user user with username and password
+     * @returns {Response}
+     */
+    this.auth = ({
+        user
+    } = {}) => {
+        user = { ...user,
+            grant_type: 'password',
+            scope: 'read write'
+        }
+        let req = Object.assign({}, AUTH_REQUEST);
+        req.data = $httpParamSerializer(user);
+        return this.post(req).then((resp) => {
+            $cookies.put("access_token", resp.data.access_token);
+            return resp;
+        });
+    };
+
+    /**
+     * GET request
+     * @param {*} path path of request
+     * @param {*} req (optional) request
+     * @returns {Response}
+     */
+    this.get = ({
+        path = ``,
+        req = Object.assign({}, REQUEST)
+    }) => {
+        const METHOD = `GET`;
+        req.method = METHOD;
+        req.url = `${req.url}${path}`;
+        return this.request(req);
+    };
+
+    /**
+     * POST request
+     * @param {*} path path of request 
+     * @param {*} data data to be send
+     * @param {*} req (optional) request
+     * @returns {Response}
+     */
+    this.post = ({
+        path = ``,
+        data = {},
+        req = Object.assign({}, REQUEST)
+    }) => {
+        const METHOD = `POST`;
+        req.method = METHOD;
+        req.data = data;
+        return this.request(req);
+    };
+
+    /**
+     * PATCH request
+     * @param {*} path path of request 
+     * @param {*} data data to be send
+     * @param {*} req (optional) request
+     * @returns {Response}
+     */
+    this.patch = ({
+        path = ``,
+        data = {},
+        req = Object.assign({}, REQUEST)
+    }) => {
+        const METHOD = `PATCH`;
+        req.method = METHOD;
+        req.data = data;
+        return this.request(req);
+    };
+
+    /**
+     * DELETE request
+     * @param {*} path path of request 
+     * @param {*} data data to be send
+     * @param {*} req (optional) request
+     * @returns {Response}
+     */
+    this.delete = ({
+        path = ``,
+        data = {},
+        req = Object.assign({}, REQUEST)
+    }) => {
+        const METHOD = `PATCH`;
+        req.method = METHOD;
+        req.data = data;
+        return this.request(req);
+    };
+
+    /**
+     * Execute request
+     * @param {*} req request
+     * @returns {Response}
+     */
+    this.request = (req) => {
+        return $http(req);
+    };
+
+    /**
+     * @returns {API}
+     */
+    return this;
+});
