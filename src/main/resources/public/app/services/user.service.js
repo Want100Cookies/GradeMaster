e41e0422683fbe235f4ff739e8952878d2b46d75
Danny Hooyer <DannyHooyer@gmail.com>
2018-01-19 12:23:26 +0100

    Moved services to designated folder, and changed script imports in index.html

--- src/main/resources/public/app/services/user.service.js
+++ src/main/resources/public/app/services/user.service.js
@@ -14,6 +14,23 @@ app.factory('UserService', function ($cookies, $q, $resource, $http, $state) {
             }).catch(function (data) {
                 console.log(data);
             });
+        },
+
+        getUser : function(){
+            var accessToken = $cookies.get("access_token");
+            var req = {
+                method: 'GET',
+                url: 'http://localhost:8080/api/v1/users/self',
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                }
+            }
+            return $http(req).then(function(data){
+                console.log("Succesful API Call", data);
+                return data;
+            }).catch(function(data){
+                console.log("ERROR", data);
+            });
         }
     }
 });
