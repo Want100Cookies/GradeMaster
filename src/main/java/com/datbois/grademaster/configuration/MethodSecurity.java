511b0a3bf07297d87aaaa4cd5502f213f7cc4c62
Pascal Drewes <pascal@drewes-webdesign.nl>
2018-01-21 19:30:31 +0100

    Get the user on every method call

--- src/main/java/com/datbois/grademaster/configuration/MethodSecurity.java
+++ src/main/java/com/datbois/grademaster/configuration/MethodSecurity.java
@@ -1,5 +1,7 @@
 package com.datbois.grademaster.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,9 +11,13 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class MethodSecurity extends GlobalMethodSecurityConfiguration {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Override
     protected MethodSecurityExpressionHandler createExpressionHandler() {
         CustomMethodSecurityExpressionHandler expressionHandler = new CustomMethodSecurityExpressionHandler();
+        expressionHandler.setApplicationContext(applicationContext);
 
         return expressionHandler;
     }
